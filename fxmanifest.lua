b1b018f209082bf284161c1fbee7e961498d3c7f
DS4L-xcyber <ds4ldevelopment@gmail.com>
2025-03-23 16:40:01 -0400

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'DS4L'
+description 'Enhanced Drift Smoke by DS4L discord.gg/P9kK7RkN34'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+} 
+dependency '/assetpacks'
